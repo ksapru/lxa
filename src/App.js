8dc21c9178e71021157e29ab0cbab5f2126c1437
Krish Sapru <ksapru@bu.edu>
2023-01-20 05:13:51 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import chess from './chess.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={chess} height="400px" width="400px" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
